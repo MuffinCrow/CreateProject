0034c61d2352ded045acf55334ea0745d3a68481
Devon Botney <dbotney@ucvts.tec.nj.us>
2019-03-22 10:29:38 -0400

    Pretty sure we finished the game

--- jack.js
+++ jack.js
@@ -8,6 +8,8 @@ var turn = 0;
 var gameOver = 0;
 var handSum = 0;
 var dealerSum = 0;
+var standTrue = 0;
+var tie = 0;
 //turn = 1; your turn
 //turn = 2; dealer turn
 //deckNum = _; the place value in the deck we are dealing from
@@ -27,6 +29,13 @@ function reset() {
   gameOver = 0;
   handSum = 0;
   dealerSum = 0;
+  standTrue = 0;
+  var tie = 0;
+  for (var i = 0; i < 10; i++) {
+    var c = document.getElementById(i + 1);
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 158, 246);
+  }
 }
 
 function shuffle() {
@@ -64,9 +73,31 @@ function deal(crd) {
   turn = crd;
   deckNum++;
   if (turn == 1) {
+    var c = document.getElementById(hand.length + 6);
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 158, 246);
+    var temp = deck[deckNum];
     hand.push(deck[deckNum]);
+    ctx.textAlign = "center";
+    ctx.textAlign = "middle";
+    ctx.font = "120px Arial";
+    ctx.strokeText(temp, 70, 170);
   } else if (turn == 2) {
+    var c = document.getElementById(dealer.length + 1);
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 158, 246)
+    var temp = deck[deckNum];
     dealer.push(deck[deckNum]);
+    if (dealer.length > 1 && standTrue == 0) {
+      var img = src="Images/cardBack.jpg";
+      ctx.fillStyle = "#0280fd";
+      ctx.fillRect(0, 0, 158, 246);
+    } else {
+      ctx.textAlign = "center";
+      ctx.textAlign = "middle";
+      ctx.font = "120px Arial";
+      ctx.strokeText(temp, 70, 170);
+    }
   } else {
     console.log("Error: Turn is not equal to 1 or 2");
   }
@@ -92,19 +123,17 @@ console.log(handSum + " is the handSum");
     console.log("MY TURN");
     if (handSum > 21) {
       convert();
-      console.log("I am changed");
     }
     if (handSum == 21) {
       //WE NEED TP ADD THE SPOT WHERE YOU WIN OR YOU LOSE GOES
       gameOver = 2;
-      console.log("I am winner");
+      alert("YOU WIN \nYour hand total: " + handSum + "\nDealer's hand total: " + dealerSum);
     }
     if (handSum > 21) {
       gameOver = 1;
-      console.log("I am greater");
+      alert("YOU LOSE\nYour hand total: " + handSum + "\nDealer's hand total: " + dealerSum);
     } else if (handSum < 21) {
       gameOver = 0;
-      console.log("I am less");
     }
     } else if (turn == 2) {
     if (dealerSum > 21) {
@@ -114,8 +143,13 @@ console.log(handSum + " is the handSum");
     }
     if (dealerSum < handSum) {
       gameOver = 2;
-    } else if (dealerSum >= handSum) {
+      alert("YOU WIN\nYour hand total: " + handSum + "\nDealer's hand total: " + dealerSum);
+    } else if (dealerSum > handSum) {
       gameOver = 1;
+      alert("YOU LOSE\nYour hand total: " + handSum + "\nDealer's hand total: " + dealerSum);
+    } else if (dealerSum == handSum) {
+      gameOver = 3;
+      alert("YOU TIED\nYour hand total: " + handSum + "\nDealer's hand total: " + dealerSum);
     }
   }
   console.log(gameOver);
@@ -151,17 +185,24 @@ function stand() {
 
   if (gameOver == 0) {
     turn = 2;
+    standTrue = 1;
+    tie = 1;
     while (dealerSum < 17) {
+      tie = 0;
       deal(2);
       endGame();
     }
+    if (tie == 1) {
+      endGame();
+    }
+    var c = document.getElementById("2");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 158, 246);
+    var temp = deck[3];
+    ctx.textAlign = "center";
+    ctx.textAlign = "middle";
+    ctx.font = "120px Arial";
+    ctx.strokeText(temp, 70, 170);
     console.log(dealer);
   }
-//  if (dealerSum <= 17) {
-//    deal(1);
-//  }
-//  if (dealerSum == 21) {
-//    gameOver = 2;
-//  }
-//  console.log(stand);
 }
